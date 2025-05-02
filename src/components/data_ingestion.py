4c5779f7bc6c4fafab7fde13f966768f5dbdac91
Himesh Parashar <himeshparashar424@gmail.com>
2025-05-02 18:04:51 +0530

    Add home page template for student exam performance prediction
    - Created home.html with a form for predicting student math scores based on various inputs including gender, ethnicity, parental education, lunch type, test preparation course, and scores.
    - Added index.html as a welcome page.

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformationConfig
 from src.components.data_transformation import DataTransformation
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts', 'train.csv')
@@ -43,6 +46,9 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
-    logging.info("Data transformation completed successfully.")
-    logging.info("Data ingestion completed successfully.")
+    train_array, test_array, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+
+    model_trainer = ModelTrainer()
+    r2_square = model_trainer.initiate_model_trainer(train_array, test_array)
+    print(f"R2 Score: {r2_square}")
+    
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,8 @@ import sys
 import numpy as np
 import pandas as pd
 import dill
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -18,3 +20,31 @@ def save_object(obj, file_path):
 
     except Exception as e:  
         raise CustomException(e, sys)
+    
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
+    """
+    Evaluate the given models and return a report of their performance.
+    """
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+
+            gs = GridSearchCV(
+                estimator=model,
+                param_grid=params[model_name],
+                cv=3
+            )
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            print(f"Best parameters for {model_name}: {gs.best_params_}")
+            model.fit(X_train, y_train)
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            report[model_name] = np.round(r2_score(y_test, y_test_pred), 2)
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys) from e
